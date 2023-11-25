350eb836cfd55da5ff2aa41316aad08153693081
rikardbq <j10rikardb@gmail.com>
2023-11-25 21:22:30 +0100

    continue later, save stream song code for another projj

--- routes/[view].tsx
+++ routes/[view].tsx
@@ -2,5 +2,5 @@ import { PageProps } from "fresh/server.ts";
 import Views from "../components/views/index.ts";
 
 export default (props: PageProps) => {
-  return Views[props.params.view];
+  return <Head>{Views[props.params.view]}</Head>;
 };
